e2f29cabe37f9064881589fd70dc30bd6c9ed776
Chiqueto <lfchiqueto@gmail.com>
2025-02-20 07:47:45 -0300

    chore: add eslint sort plugin and prettier tailwind plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import simpleImportSort from "eslint-config-simple-import-sort";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -16,9 +16,10 @@ const eslintConfig = [
     plugins: {
       "simple-import-sort": simpleImportSort,
     },
-    rules: {},
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
   },
 ];
 
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@/components/ui/button";
 
 const ProductPage = () => {
-    return (<div>
-        <h1>Products Page</h1>
-        <Button>FSW 7.0</Button>
-    </div>);
+    return (
+        <div className="border rounded-xl p-5 border-red-500">
+            <h1>Products Page</h1>
+            <Button>FSW 7.0</Button>
+        </div>
+    );
 }
 
-export default ProductPage;
+export default ProductPage;  
