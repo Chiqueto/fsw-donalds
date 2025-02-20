7187276ddf1145ae96e9fa452da8e9232f61278a
Chiqueto <lfchiqueto@gmail.com>
2025-02-20 08:44:10 -0300

    chore: try to fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import { FlatCompat } from '@eslint/eslintrc';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,15 +11,25 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
   {
     plugins: {
-      "simple-import-sort": simpleImportSort,
+      'simple-import-sort': simpleImportSort,
+      prettier: 'eslint-plugin-prettier', // Adiciona o plugin do Prettier
     },
     rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          plugins: ['prettier-plugin-tailwindcss'], // Adiciona o plugin do Tailwind CSS
+        },
+      ],
     },
+  },
+  {
+    extends: ['plugin:prettier/recommended'], // Adiciona a configuração do Prettier
   },
 ];
 
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,11 +2,11 @@ import { Button } from "@/components/ui/button";
 
 const ProductPage = () => {
     return (
-        <div className="border rounded-xl p-5 border-red-500">
+        <div className="rounded-xl border border-red-500 p-5 text-lg font-bold">
             <h1>Products Page</h1>
             <Button>FSW 7.0</Button>
         </div>
     );
-}
+};
 
-export default ProductPage;  
+export default ProductPage;
